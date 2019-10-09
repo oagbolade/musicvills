35348eae9e13c6090d1350733d0b0b2433835494
oagbolade <you@example.com>
2019-10-09 18:31:11 +0100

    Built using webpack production

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-
 app.use(express.static(path.join(__dirname, "dist")));
-app.set("port", process.env.PORT || 8080);
 
-const server = app.listen(app.get("port"), function() {
-  console.log("listening on port ", server.address().port);
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`App listening on ${port}`);
